f05dde7982069fcbdc303d80434f87231febcd3c
Peterson Sena <petesonsena24@gmail.com>
2025-03-22 00:38:02 +0100

    l

--- src/pages/api/chat.ts
+++ src/pages/api/chat.ts
@@ -1,5 +1,4 @@
 import type { APIRoute } from 'astro';
-export const prerender = false;
 export const POST: APIRoute = async ({ request }) => {
   try {
     const formData = await request.formData();
